2a4f14a6f1ffdbb676eb2f5857d51c3e4d0f029a
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-11 18:44:07 -0400

    Fixed index issue

---
+++ pages/[Major]/[Course]/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Course: NextPage = () => {
+    return (
+        <h1> Course </h1>
+    )
+}
+
+export default Course
---
+++ pages/[Major]/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Major: NextPage = () => {
+    return (
+        <h1> Major </h1>
+    )
+}
+
+export default Major
