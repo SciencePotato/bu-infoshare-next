6e7208281c8d6c621fff052c0ab32656cbee9c83
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-13 13:44:12 -0400

    Started the based of the HomePage

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,7 +1,26 @@
+import styles  from '../../styles/Nav.module.scss';
+import logo from '../../public/infoshare.png';
+import Image from 'next/image'
+
 export default function Navbar () {
     return (
         <>
-            <h1> Navbar </h1>
+            <nav className={styles.nav}>
+                {/* Logo flex: 1 */}
+                <div>
+                    <Image src={logo}/>
+                </div>
+
+                {/* Search flex: 6 */}
+                <div>
+                </div>
+
+                {/* Others flex: 1-2? */}
+                <div>
+                    <div> Course </div>
+                    <div> Majors </div>
+                </div>
+            </nav>
         </>
     )
 }
