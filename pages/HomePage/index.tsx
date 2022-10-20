5ae8ddf9199a89e048976693d7c90ffc1ce933ca
Joseph Jang <josephjang11@gmail.com>
2022-10-20 19:06:41 -0400

    Change Post Question to Component

--- pages/HomePage/index.tsx
+++ pages/HomePage/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 import Navbar from '../../components/navbar/navbar'
 import styles from '../../styles/HomePage.module.scss'
 import Post from '../../components/post/Post'
+import PostQ from '../../components/postQ/postquestion'
 
 
 const Home: NextPage = () => {
@@ -30,13 +31,7 @@ const Home: NextPage = () => {
         {/* Posts */}
         <section>
           {/* Post a Question */}
-          <div className={styles.postQ}>
-              <div> Post a Question</div>
-              <div>
-                <span> Video </span>
-                <span> Photo </span>
-              </div>
-          </div>
+          <PostQ></PostQ>
           
           <Post></Post>
           <Post></Post>
