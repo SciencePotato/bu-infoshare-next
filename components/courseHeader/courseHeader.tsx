980acd0e656b53e89b3e6cde8824703bbd91cb87
Joseph Jang <josephjang11@gmail.com>
2022-10-20 23:10:30 -0400

    Finished Course Page

--- components/courseHeader/courseHeader.tsx
+++ components/courseHeader/courseHeader.tsx
@@ -4,14 +4,20 @@ import Image from 'next/image'
 export default function courseHeader() {
     return (
         <>
+
+            <div className={styles.line}></div>
+
             <div className={styles.course_title}>
+
+
                 {/* Course ID */}
                 <div>
                     <div> CS 320 </div>
                 </div>
+
                 {/* Course Name */}
                 <div>
-                    <div> Principals of Programming Language </div>
+                    <div> Concepts of Programming Languages </div>
                 </div>
             
             </div>
