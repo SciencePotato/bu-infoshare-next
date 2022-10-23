4605fdd7ab60946349741815eddac74157813dfa
Joseph Jang <josephjang11@gmail.com>
2022-10-23 13:34:09 -0400

    Create Post Question Modal

--- components/postQ/postquestion.tsx
+++ components/postQ/postquestion.tsx
@@ -1,16 +1,27 @@
 import styles  from '../../styles/PostQ.module.scss';
 import Image from 'next/image'
+import { useState } from 'react';
+import Modal from '../../components/modal/postQmodal'
+
 
 export default function Post() {
+
+    const[postqModal, setPostqmodal] = useState(false)
+
     return (
         <>
-            <div className={styles.postQ}>
-                  <div> Post a Question</div>
+
+            <div className={styles.postContainer}>
+                  <form>
+                    <input type="text" onClick={() => setPostqmodal(true)} placeholder='Post a Question' className={styles.postqText} 
+                    ></input>
+                  </form>
                   <div>
                     <span> Video </span>
                     <span> Photo </span>
                   </div>
-              </div>
+            </div>
+            {postqModal && <Modal closeModal={setPostqmodal} />}
         </>
     )
 }
