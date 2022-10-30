161073d18e9986638de3dc7ee22a2c44e6d07709
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-29 22:27:37 -0400

    new test

--- pages/HomePage/index.tsx
+++ pages/HomePage/index.tsx
@@ -8,6 +8,14 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase, ref, onValue, get, child } from 'firebase/database';
 import React, { useEffect, useState } from 'react';
 
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API,
+  authDomain: process.env.FIREBASE_AUTHDOM,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGE,
+}
+
 const Home: NextPage<any> = ({dataArray}) => {
   console.log(process.env.FIREBASE_API)
   console.log(process.env.FIREBASE_AUTHDOM)
@@ -61,14 +69,7 @@ const Home: NextPage<any> = ({dataArray}) => {
 
 export default React.memo(Home)
 
-export async function getStaticProps() {
-  console.log("1")
-  console.log(process.env.FIREBASE_API)
-  console.log(process.env.FIREBASE_AUTHDOM)
-  console.log(process.env.FIREBASE_DB_URL)
-  console.log(process.env.FIREBASE_PROJECTID)
-  console.log(process.env.FIREBASE_STORAGE)
-
+export async function getStaticProps(firebaseConfig: any) {
   // const firebaseConfig = {
   //     apiKey: process.env.FIREBASE_API,
   //     authDomain: process.env.FIREBASE_AUTHDOM,
@@ -77,13 +78,13 @@ export async function getStaticProps() {
   //     storageBucket: process.env.FIREBASE_STORAGE,
   // }
 
-  const firebaseConfig = {
-    apiKey: "AIzaSyD-sgjpJ5oJr1lbD7oxlgPdZbQxESPWXdw",
-    authDomain: "buinfoshare.firebaseapp.com",
-    databaseURL: "https://buinfoshare-default-rtdb.firebaseio.com/",
-    projectId: "buinfoshare",
-    storageBucket: "buinfoshare.appspot.com",
-  };
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyD-sgjpJ5oJr1lbD7oxlgPdZbQxESPWXdw",
+  //   authDomain: "buinfoshare.firebaseapp.com",
+  //   databaseURL: "https://buinfoshare-default-rtdb.firebaseio.com/",
+  //   projectId: "buinfoshare",
+  //   storageBucket: "buinfoshare.appspot.com",
+  // };
 
   const app = initializeApp(firebaseConfig)
   const database = getDatabase(app);
