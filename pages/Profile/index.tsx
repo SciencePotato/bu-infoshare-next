0836380cf0858a23533832c21db15bc0b315f802
Joseph Jang <josephjang11@gmail.com>
2022-10-30 16:19:03 -0400

    Fix

--- pages/Profile/index.tsx
+++ pages/Profile/index.tsx
@@ -35,7 +35,7 @@ const Profile: NextPage = () => {
           <div className={styles.bio}>
 
             <div className={styles.bioHead}>Bio</div>
-            <div className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambledit to make it a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
+            <div className={styles.description}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a gallery of type and scrambledit to make it a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>
 
           </div>
         </div>
