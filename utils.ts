25922612dcf649f5ef58938a4c1d1c4f94178dc1
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-10-30 22:22:00 -0400

    encryption nth try

--- utils.ts
+++ utils.ts
@@ -5,3 +5,12 @@ export const firebaseConfig = {
     projectId: "buinfoshare",
     storageBucket: "buinfoshare.appspot.com",
 }
+
+export const firebaseConfigEncrypted = {
+    apikey: process.env.NEXT_PUBLIC_FIREBASE_API,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOM,
+    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DB_URL,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+
+}
