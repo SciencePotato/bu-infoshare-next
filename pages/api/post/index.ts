4321a58e446a7f64f42b27a4258e2db0a0b97e31
Hou <64312205+SciencePotato@users.noreply.github.com>
2022-11-04 13:57:58 -0400

    e

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -7,22 +7,17 @@ import { getDatabase, ref, get, set, child } from 'firebase/database';
 type Data = {
   name: string
 }
-const updateFirebase:any = async () => {
-  const app = initializeApp(firebaseConfig)
-  const database = getDatabase(app);
-  set(ref(database, 'maxPost'), {
-    num: Math.random() * 100
-  })
-}
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
     if (req.method === 'POST') {
-      let update = await updateFirebase();
-      update.then(() => 
-        res.status(201).json(req.body)
-      )
+      const app = initializeApp(firebaseConfig)
+      const database = getDatabase(app);
+      await set(ref(database, 'maxPost'), {
+        num: Math.random() * 100
+      })
+      res.status(201).json(req.body)
     }
 }
