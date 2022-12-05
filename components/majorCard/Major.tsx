09fc1851a262190b7363762fd26582199c814583
jojang <josephjang11@gmail.com>
2022-12-04 19:26:12 -0500

    update major

--- components/majorCard/Major.tsx
+++ components/majorCard/Major.tsx
@@ -31,6 +31,82 @@ const MajorCard: NextPage<Props> = ({data, pathID}) => {
         major = "Data Science"
     } else if (major == "computerengineering") {
         major = "Computer Engineering"
+    } else if (major == "acting") {
+        major = "Acting/ Theatre Arts"
+    } else if (major == "aerospace") {
+        major = "Aerospace Engineering"
+    } else if (major == "africanamericanstudies") {
+        major = "African American Studies"
+    } else if (major == "americanstudies") {
+        major = "American Studies"
+    } else if (major == "asianstudies") {
+        major = "Asian Studies"
+    } else if (major == "behaviorandhealth") {
+        major = "Behavior and Health"
+    } else if (major == "biomedical") {
+        major = "Biomedical Engineering"
+    } else if (major == "cinema") {
+        major = "Cinema & Media Studies"
+    } else if (major == "civilization") {
+        major = "Classical Civilization"
+    } else if (major == "literature") {
+        major = "Comparative Literature"
+    } else if (major == "composition") {
+        major = "Composition & Theory"
+    } else if (major == "costume") {
+        major = "Costume Design"
+    } else if (major == "environment") {
+        major = "Environmental Science"
+    } else if (major == "electrical") {
+        major = "Electrical Engineering"
+    } else if (major == "european") {
+        major = "European Studies"
+    } else if (major == "film") {
+        major = "Film & Television"
+    } else if (major == "graphic") {
+        major = "Graphic Design"
+    } else if (major == "health") {
+        major = "Health Science"
+    } else if (major == "hospitality") {
+        major = "Hospitality Administration"
+    } else if (major == "physiology") {
+        major = "Human Physiology"
+    } else if (major == "ir") {
+        major = "International Relations"
+    } else if (major == "latinamerican") {
+        major = "Latin American Studies"
+    } else if (major == "lightingdesign") {
+        major = "Lighting Design"
+    } else if (major == "manufacturing") {
+        major = "Manufacturing Engineering"
+    } else if (major == "marine") {
+        major = "Marine Science"
+    } else if (major == "mechanical") {
+        major = "Mechanical Engineering"
+    } else if (major == "media") {
+        major = "Media Science"
+    } else if (major == "physicaltherapy") {
+        major = "Physical Therapy"
+    } else if (major == "political") {
+        major = "Political Science"
+    } else if (major == "publicrelations") {
+        major = "Public Relations"
+    } else if (major == "middleeast") {
+        major = "Middle East South Asian Literatures"
+    } else if (major == "theaterprod") {
+        major = "Theater Production"
+    } else if (major == "theatrearts") {
+        major = "Theatre Arts"
+    } else if (major == "sounddesign") {
+        major = "Sound Design"
+    } else if (major == "stagemanagement") {
+        major = "Stage Management"
+    } else if (major == "scenedesign") {
+        major = "Scene Design"
+    } else if (major == "scienceeducation") {
+        major = "Science Education"
+    } else if (major == "speech") {
+        major = "Speech, Language & Hearing Sciences"
     } else {
         major = major.charAt(0).toUpperCase() + major.slice(1).toLowerCase()
     }
